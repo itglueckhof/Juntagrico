b06c98f3c3f6614b1dadeb3617367886176df545
itglueckhof <124088077+itglueckhof@users.noreply.github.com>
2023-03-10 13:28:18 +0100

    Update settings.py

--- glueckhof/settings.py
+++ glueckhof/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get('JUNTAGRICO_SECRET_KEY')
 
 DEBUG = os.environ.get("JUNTAGRICO_DEBUG", 'False')=='True'
 
-ALLOWED_HOSTS = ['glueckhof.juntagrico.science', 'localhost',]
+ALLOWED_HOSTS = ['glueckhof.juntagrico.science', 'localhost', 'mein.glueck-hof.ch']
 
 
 # Application definition
