18bb5872dfd7103e69b700c13c74dd11761e93c8
itglueckhof <124088077+itglueckhof@users.noreply.github.com>
2023-04-09 15:52:11 +0200

    Update settings.py

--- glueckhof/settings.py
+++ glueckhof/settings.py
@@ -201,8 +201,8 @@ IMAGES = {'status_100': '/static/glueckhof/img/Kleeblatt_100.png',
 
 # Anpassung der Emails
 EMAILS = {
-    'welcome': 'mails/member/member_welcome.txt',
-    'co_welcome': 'mails/member/co_member_welcome.txt',
+    'welcome': 'mails/member_welcome.txt',
+    'co_welcome': 'mails/co_member_welcome.txt',
     's_created': 'mails/share_created.txt',
 }
 
