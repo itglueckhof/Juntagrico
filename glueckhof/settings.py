3c4541ba176590cc9a64139bd52d2e38dbf7b347
itglueckhof <124088077+itglueckhof@users.noreply.github.com>
2024-06-22 18:22:21 +0200

    Update settings.py

--- glueckhof/settings.py
+++ glueckhof/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'glueckhof',
     'juntagrico',
     'fontawesomefree',
     'import_export',
