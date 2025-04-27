6a4dbd26d7f1bdeee7fba96ceb9ac06de5e874dc
itglueckhof <124088077+itglueckhof@users.noreply.github.com>
2025-04-27 14:54:07 +0200

    Update settings.py

--- glueckhof/settings.py
+++ glueckhof/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.tz',
                 'django.template.context_processors.request',
                 'django.contrib.messages.context_processors.messages',
+                'juntagrico.context_processors.vocabulary',
             ],
             'debug': DEBUG
         },
